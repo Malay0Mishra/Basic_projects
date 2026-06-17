3c4503a157ef0c786d3ca04a8239da03d2bcb4ae
Malay0Mishra <malaymishra875@gmail.com>
2026-06-17 09:20:21 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,11 +3,20 @@ import json
 with open("data.json", "r") as file:
     data = json.load(file)
 
+
+
 def load_data():
-    return data.json 
+    with open("data.json", "r") as file:
+        return json.load(file)
+
+
 
 def save_data(data):
-    return data.json 
+    with open("data.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+
+
 
 def register_shop(): 
     name = input("Enter your Shop Name : ")
@@ -19,3 +28,20 @@ def register_shop():
         print(f"{name} is registered successfully")
         save_data(data)
         return name 
+
+
+def add_product():
+    name = input("Enter your Shop Name to Verify :  ")
+    if name in data:
+        print(f"{name} is Shop Verified")
+        product = input("Enter product name: ")
+        price = int(input("Enter price: "))
+        quantity = int(input("Enter quantity: "))
+        if product in data[name]:
+            print(f"{product} is already added")
+        else:
+            data[name][product] = {"price": price, "quantity": quantity}
+            print(f"{product} is added successfully")
+            save_data(data)
+    else:
+        print(f"{name} is not shop verified")
